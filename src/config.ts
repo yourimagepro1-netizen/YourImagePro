acedbc557bcff22bc38c3dbae7ba4cdbb5d624bf
yourimagepro1-netizen <yourimagepro1@gmail.com>
2025-10-02 10:53:58 -0400

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  API_BASE_URL: https://rollbackyourimage-169263341271.us-west1.run.app, 
+  STRIPE_PUBLISHABLE_KEY: pk_live_51S9oDh3ZHa8jpsiXHo3KTnW4xzOynB3jYMaPnNrTBuemHd9zdhr25xiq2ElYm53fNEGlHcw1Sgc26s5bSjMxcQX600deL9opbE,
+  GOOGLE_CLIENT_ID: 169263341271-3pra7mcnk85vatb0rmk9dkui835geo09.apps.googleusercontent.com,
+  // add more keys here if your app asks for them
+};
